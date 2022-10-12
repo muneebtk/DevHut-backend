8671e698b6f21c30148d6ef60e7baaa141ab28ed
muhammed muneeb <103311072+muneebtk@users.noreply.github.com>
2022-10-12 18:19:37 +0530

    'four'

--- devhut/devhut/settings.py
+++ devhut/devhut/settings.py
@@ -234,7 +234,7 @@ AWS_DEFAULT_ACL = 'public-read'
 AWS_LOCATION = 'static'
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 STATICFILES_DIRS=[
-  os.path.join(BASE_DIR, 'media'),
+    'static'
 ]
 STATIC_URL = 'http://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
